46ad56ce0bd28211169a64c7d22af916cd59b53a
Teddy Minaya <teddyminaya@gmail.com>
2023-01-19 10:50:04 -0500

    commit tree 2bacdfea2c46fb43d013962bf33ff569caec1603

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+//TESTS
+    
+  function submitData(name,email){
+    return fetch(`http://localhost:3000/users`,{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify({
+          name:`${name}`,
+          email:`${email}`,   
+        }),
+    })
+    .then(res => res.json())
+    .then(object => document.body.innerHTML= object.id)
+    .catch(error => document.body.innerHTML= error.message)
+}
